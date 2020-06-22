900483d3a395f9142df2db8bd0a8cf61e7b7144c
301083645 <67066443+301083645@users.noreply.github.com>
2020-06-22 11:19:47 -0400

    added transform

---
+++ COMP123-M2020-Eunbee2/Vector2D.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace COMP123_M2020_Eunbee2
+{
+    struct Vector2D
+    {
+
+        public float a1;
+        public float a2;
+    }
+}
