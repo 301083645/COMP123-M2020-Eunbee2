1d0a1054dce7c24409e766fe52b771a4bd08d7f4
301083645 <67066443+301083645@users.noreply.github.com>
2020-06-22 11:21:38 -0400

    add transform

---
+++ COMP123-M2020-Eunbee2/Transform.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace COMP123_M2020_Eunbee2
+{
+    struct Transform
+    {
+
+        public Vector2D position;
+        public Vector2D Rotation;
+        public Vector2D Scale;
+    }
+}
